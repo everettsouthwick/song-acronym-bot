e9f15e6ee8106af7204f45475197a0b3d73995a1
Everett Southwick <everett@southwick.dev>
2020-10-09 21:38:49 -0400

    Track submissions and comments

--- modules/helper.py
+++ modules/helper.py
@@ -1,6 +1,98 @@
-# import praw
+from .models import Blacklist, Comment, Keyword
+from .repository import add_comment, author_is_blacklisted, comment_is_replied, get_all_keywords
+import praw
+import time
 
-# def is_summon_chain(reddit, comment : praw.models.Comment):
-#     if not comment.is_root:
-#         parent_comment_id = comment.parent_id
-#         parent_comment = reddit.get_info(thing_id = parent_comment_id)
+# Initialize list of keywords we are looking for.
+keywords = get_all_keywords()
+
+def submissions_and_comments(subreddit, **kwargs):
+    results = []
+    results.extend(subreddit.new(**kwargs))
+    results.extend(subreddit.comments(**kwargs))
+    results.sort(key=lambda post: post.created_utc, reverse=True)
+    return results
+
+def process_post(post):
+    if not should_comment(post):
+        return
+
+    # If this post has a title property then it is a submission.
+    if hasattr(post, 'title'):
+        print(f"SUBMISSION {post.permalink} :: {post.title}")
+        process_submission(post)
+
+    # Otherwise, this post is a comment.
+    else:
+        print(f"COMMENT {post.permalink} :: {post.body}")
+        process_comment(post)
+
+def process_comment(comment : praw.models.Comment):
+    reply_text = format_reply(comment.body.lower())
+
+    if reply_text != "":
+        try:
+            comment.reply(reply_text)
+            add_comment(comment.id, 1)
+        except:
+            print("Processing of comment failed!")
+
+def process_submission(submission : praw.models.Submission):
+    reply_text = format_reply(f"{submission.title.lower()} {submission.selftext.lower()}")
+
+    if reply_text != "":
+        try:
+            submission.reply(reply_text)
+            add_comment(submission.id, 1)
+        except:
+            print("Processing of submission failed!")
+
+
+
+def should_comment(post):
+    if post.author.name == "songacronymbot":
+        return False
+
+    if time.time() - post.created_utc > 300:
+        return False
+
+    if author_is_blacklisted(post.author.name):
+        print(f"{post.author.name} is blacklisted!")
+        return False
+    
+    if comment_is_replied(post.id):
+        print(f"{post.id} has already been replied to!")
+        return False
+
+    return True
+
+def format_reply(text : str):
+    reply_text = get_comment_text(text)
+
+    if reply_text != "":
+        return add_footer(reply_text)
+
+    return reply_text
+
+def get_comment_text(text : str):
+    comment_text = ""
+    for keyword in keywords:
+        match = find_match(text, f" {keyword.keyword.lower()} ")
+
+        if match == -1:
+            match = find_match(text, f" {keyword.keyword.lower()}:")
+
+        if match == -1:
+            match = find_match(text, f"\"{keyword.keyword.lower()}\"")
+
+        if match != -1:
+            comment_text += f"- {keyword.comment_text}\n"
+            
+    return comment_text
+
+def find_match(text, keyword):
+    return text.find(keyword)
+
+def add_footer(text: str):
+    return f"{text}\n---\n\n^(_I am a bot. | DM for inquiries/feedback/opt-out._)"
+
--- songacronymbot.py
+++ songacronymbot.py
@@ -1,70 +1,14 @@
-from modules.models import Blacklist, Comment, Keyword
-import modules.repository as Repo
+import modules.helper as Helper
 import praw
-import time
 
 # Initialize Reddit instance.
-reddit = praw.Reddit("bot1", user_agent="script:songacronymbot:v1.0 (by u/everdoot)")
+r = praw.Reddit("songacronymbot")
 
-# Initialize list of keywords we are looking for.
-keywords = Repo.get_all_keywords()
-
-def should_comment(comment : praw.models.Comment):
-    if comment.author.name == "songacronymbot":
-        print("This is your own comment!")
-        return False
-
-    if time.time() - comment.created_utc > 600:
-        print("Comment is greater than 10 minutes old!")
-        return False
-
-    if Repo.author_is_blacklisted(comment.author.name):
-        print(f"{comment.author.name} is blacklisted!")
-        return False
+def stream():
+    subreddit = r.subreddit('taylorswift')
+    stream = praw.models.util.stream_generator(lambda **kwargs: Helper.submissions_and_comments(subreddit, **kwargs))
     
-    if Repo.comment_is_replied(comment.id):
-        print(f"{comment.id} has already been replied to!")
-        return False
+    for post in stream:
+        Helper.process_post(post)
 
-    return True
-
-def format_reply(body : str):
-    reply_text = ""
-    for keyword in keywords:
-        match = body.find(f" {keyword.keyword.lower()} ")
-
-        if match != -1:
-            if reply_text == "":
-                reply_text = f"> {keyword.comment_text}"
-            else:
-                reply_text = f"{reply_text}\n> {keyword.comment_text}"
-
-    return add_footer(reply_text)
-
-def add_footer(reply_text: str):
-    if reply_text == "":
-        return reply_text
-
-    return f"{reply_text}\n___\n^(I am a bot. | DM to be removed from automatic replies.)"
-
-def crawl():
-    for comment in reddit.subreddit('taylorswift').stream.comments():
-        comment_id = comment.id
-        comment_body = comment.body.lower()
-
-        if not should_comment(comment):
-            continue
-
-        reply_text = format_reply(comment_body)
-
-        if reply_text != "":
-            try:
-                print("I replied to '" + comment_body + "' with " + reply_text)
-                comment.reply(reply_text)
-                Repo.add_comment(comment_id, 1)
-                time.sleep(601)
-            except:
-                print("Error!")
-                break
-
-crawl()
+stream()
